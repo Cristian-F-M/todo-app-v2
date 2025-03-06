df38056c753d9d7cb0e9ec616125b428e3263f40
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-06 16:10:59 -0500

    Add global CSS import: include global styles for consistent theming and design across the application layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Slot } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { View } from 'react-native'
+import '../global.css'
 
 export default function RootLayout() {
   return (
