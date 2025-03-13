89cb2cfd18c8f87b17818033cd4c6a34c3d033d3
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-13 17:15:19 -0500

    Implement folder management UI: integrate folder creation modal and list display with styled components for improved user experience

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,45 @@
-import { Text, View } from 'react-native'
+import { Screen } from '@components/Screen'
+import { Header } from '@components/Header'
+import { FlatList, View } from 'react-native'
+import { StyledPressable } from '@components/StyledPressable'
+import { useCallback } from 'react'
+import { BackgroundIcon } from '@components/BackgroundIcon'
+import { FolderItem } from '@components/FolderItem'
+import { useTasks } from '@context/Tasks'
+import { useModal } from '@context/Modal'
 
 export default function Index() {
-  return <View></View>
+  const { folders } = useTasks()
+  const { openModal } = useModal()
+
+  const openCreateFolderModal = useCallback(
+    (e?: any) => {
+      openModal(e, {
+        type: 'FOLDER',
+        mode: 'CREATE',
+      })
+    },
+    [openModal],
+  )
+
+  return (
+    <Screen>
+      <BackgroundIcon />
+      <Header />
+      <View className="w-1/4 mx-auto flex-row items-center justify-center">
+        <StyledPressable
+          text="Agregar"
+          onPress={openCreateFolderModal}
+        />
+      </View>
+      <View className="mt-10 items-center justify-center w-11/12 mx-auto">
+        <FlatList
+          className="w-full gap-y-2"
+          data={folders}
+          renderItem={({ item }) => <FolderItem folder={item} />}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    </Screen>
+  )
 }
