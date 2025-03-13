47082f8ac0c6bf953d1c322af6978e6bf4b17a1d
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-13 17:15:12 -0500

    Wrap RootLayout component with providers for tasks and modal, enhancing state management and gesture handling in the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,22 @@
 import { Slot } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { View } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { TasksProvider } from '@context/Tasks'
+import { ModalProvider } from '@context/Modal'
 import '../global.css'
 
 export default function RootLayout() {
   return (
-    <View className="flex-1">
-      <StatusBar style="light" />
-      <Slot />
-    </View>
+    <GestureHandlerRootView>
+      <TasksProvider>
+        <ModalProvider>
+          <View className="flex-1">
+            <StatusBar style="light" />
+            <Slot />
+          </View>
+        </ModalProvider>
+      </TasksProvider>
+    </GestureHandlerRootView>
   )
 }
