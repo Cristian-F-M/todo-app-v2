d9762d396ec46c5df57eca9d2ce4acc6adb148b2
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-14 18:47:22 -0500

    Refactor FolderItem: replace Pressable with Link for navigation, update dropdown structure, and improve UI layout for consistency

--- components/FolderItem.tsx
+++ components/FolderItem.tsx
@@ -9,7 +9,7 @@ import Edit from '@icons/Edit'
 import { useModal } from '@context/Modal'
 import Trash from '@icons/Trash'
 import { DeleteItem } from './DeleteItem'
-import { router } from 'expo-router'
+import { Link } from 'expo-router'
 
 export function FolderItem({ folder }: { folder: FolderType }) {
   const [dropDownVisible, setDropDownVisible] = useState(false)
@@ -46,49 +46,54 @@ export function FolderItem({ folder }: { folder: FolderType }) {
   }
 
   return (
-    <Pressable
+    <Link
+      href={`/folder/${folder.id}`}
+      asChild
       className="flex-row bg-gray-800 px-4 py-4 mb-3 h-16 rounded-lg justify-between active:bg-gray-700"
-      onPress={() => router.push(`/folder/${folder.id}`)}
     >
-      <View className="flex flex-row gap-x-2 items-center justify-center">
-        <Folder stroke="#2563eb" />
-        <Text className="text-white text-lg tracking-wider items-center justify-center">
-          {folder.name}
-        </Text>
-        <Text className="text-xs text-gray-400">{folder.taskCount} tareas</Text>
+      <View className="flex-1 items-center justify-center">
+        <View className="flex flex-row gap-x-2 items-center justify-center">
+          <Folder stroke="#2563eb" />
+          <Text className="text-white text-lg tracking-wider items-center justify-center">
+            {folder.name}
+          </Text>
+          <Text className="text-xs text-gray-400">
+            {folder.taskCount} tareas
+          </Text>
+        </View>
+        <DropdownMenu
+          itemId={folder.id}
+          handleOpen={openDropdown}
+          handleClose={closeDropdown}
+          visible={dropDownVisible}
+          trigger={
+            <Pressable
+              className="active:bg-[#4b5563] p-1 rounded-lg"
+              onPress={() => setDropDownVisible(true)}
+            >
+              <MoreVertical stroke="white" />
+            </Pressable>
+          }
+        >
+          <DropdownOption
+            handleClose={closeDropdown}
+            handleOpen={openDropdown}
+            text="Editar"
+            icon={Edit}
+            onPress={openEditModal}
+            iconProps={{ stroke: 'white', width: 22, height: 22 }}
+          />
+          <DropdownOption
+            handleClose={closeDropdown}
+            handleOpen={openDropdown}
+            text="Eliminar"
+            textClassName={'!text-red-400'}
+            icon={Trash}
+            iconProps={{ stroke: '#ff6467', width: 22, height: 22 }}
+            onPress={openDeleteModal}
+          />
+        </DropdownMenu>
       </View>
-      <DropdownMenu
-        itemId={folder.id}
-        handleOpen={openDropdown}
-        handleClose={closeDropdown}
-        visible={dropDownVisible}
-        trigger={
-          <Pressable
-            className="active:bg-[#4b5563] p-1 rounded-lg"
-            onPress={() => setDropDownVisible(true)}
-          >
-            <MoreVertical stroke="white" />
-          </Pressable>
-        }
-      >
-        <DropdownOption
-          handleClose={closeDropdown}
-          handleOpen={openDropdown}
-          text="Editar"
-          icon={Edit}
-          onPress={openEditModal}
-          iconProps={{ stroke: 'white', width: 22, height: 22 }}
-        />
-        <DropdownOption
-          handleClose={closeDropdown}
-          handleOpen={openDropdown}
-          text="Eliminar"
-          textClassName={'!text-red-400'}
-          icon={Trash}
-          iconProps={{ stroke: '#ff6467', width: 22, height: 22 }}
-          onPress={openDeleteModal}
-        />
-      </DropdownMenu>
-    </Pressable>
+    </Link>
   )
 }
