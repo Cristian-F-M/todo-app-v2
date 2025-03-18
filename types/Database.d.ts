34e250412585f7d5947d7e190e6e96f0bd7aa2a3
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-18 18:08:03 -0500

    Add Database type definitions: define folder and task operations for improved type safety and consistency in the application

---
+++ types/Database.d.ts
@@ -0,0 +1,21 @@
+import type { Folder } from 'Folder'
+import type { Task } from 'Task'
+
+type retunType = Promise<{ ok: boolean }>
+
+export type CreateFolderFC = (folder: Folder) => retunType
+
+export type UpdateFolderFC = (
+  id: Folder['id'],
+  name: Folder['name'],
+) => retunType
+
+export type DeleteFolderFC = (id: Folder['id']) => retunType
+
+export type CreateTaskFC = (task: Task) => retunType
+
+export type UpdateTaskFC = (taskId: Task['id'], name: Task['name']) => retunType
+
+export type DeleteTaskFC = (id: Task['id']) => retunType
+
+export type AumentTaskCountFC = (id: Folder['id']) => retunType
