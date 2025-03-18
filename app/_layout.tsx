985768b124b2640d56b07e46fea4dd8fff6e52e6
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-18 18:29:07 -0500

    Wrap App with SQLiteProvider: integrate SQLite for database initialization within the root layout for improved data management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,23 +5,30 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { TasksProvider } from '@context/Tasks'
 import { ModalProvider } from '@context/Modal'
 import '../global.css'
+import { SQLiteProvider } from 'expo-sqlite'
+import { initDatabase } from '@utils/database'
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView>
-      <TasksProvider>
-        <ModalProvider>
-          <View className="flex-1 bg-gray-950">
-            <StatusBar style="light" />
-            <Stack
-              screenOptions={{
-                headerShown: false,
-                animation: 'slide_from_right',
-              }}
-            />
-          </View>
-        </ModalProvider>
-      </TasksProvider>
+      <SQLiteProvider
+        databaseName="todo-cm.db"
+        onInit={initDatabase}
+      >
+        <TasksProvider>
+          <ModalProvider>
+            <View className="flex-1 bg-gray-950">
+              <StatusBar style="light" />
+              <Stack
+                screenOptions={{
+                  headerShown: false,
+                  animation: 'slide_from_right',
+                }}
+              />
+            </View>
+          </ModalProvider>
+        </TasksProvider>
+      </SQLiteProvider>
     </GestureHandlerRootView>
   )
 }
