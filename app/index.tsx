7f7deebc647d8e1162700e42e3c86614c3aa06f6
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-18 19:22:54 -0500

    Remove unused modal handling in Index component and simplify NoFolders rendering logic for better clarity

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,22 @@
 import { Screen } from '@components/Screen'
 import { Header } from '@components/Header'
-import { FlatList, Text, View } from 'react-native'
-import { StyledPressable } from '@components/StyledPressable'
-import { useCallback, useEffect } from 'react'
+import { FlatList, View } from 'react-native'
 import { BackgroundIcon } from '@components/BackgroundIcon'
 import { FolderItem } from '@components/FolderItem'
 import { useTasks } from '@context/Tasks'
-import { useModal } from '@context/Modal'
 import { NoFolders } from '@components/NoFolders'
 
 export default function Index() {
   const { folders } = useTasks()
-  const { openModal } = useModal()
 
   const thereIsFolders = folders.length > 0
-
-  const openCreateFolderModal = useCallback(
-    (e?: any) => {
-      openModal(e, {
-        type: 'FOLDER',
-        mode: 'CREATE',
-      })
-    },
-    [openModal],
-  )
 
   return (
     <Screen>
       <BackgroundIcon />
       <Header />
 
-      {!thereIsFolders && <NoFolders openModal={openCreateFolderModal} />}
+      {!thereIsFolders && <NoFolders />}
 
       {thereIsFolders && (
         <View className="mt-10 items-center justify-center w-11/12 mx-auto flex-1 mb-6">
