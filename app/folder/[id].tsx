a2b3faf373e739a3fe38d1bd281f05a947ec73ac
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-19 13:48:04 -0500

    Remove console.log for tasks in Folder component to clean up unnecessary logging and improve code readability

--- app/folder/[id].tsx
+++ app/folder/[id].tsx
@@ -40,8 +40,6 @@ export default function Folder() {
   useEffect(() => {
     const tasks = getTasksByFolderId(folderId)
 
-    console.log({ tasks })
-
     setTasks(tasks)
     setTaskCount(tasks.length)
   }, [tasksFromContext, folderId, getTasksByFolderId])
