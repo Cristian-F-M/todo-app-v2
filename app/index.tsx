a7fe29a13835b0a2621da29ee8f0cae8aef46749
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-18 19:20:26 -0500

    Implement conditional rendering for folder list: show 'NoFolders' component when no folders exist in the tasks context

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,19 @@
 import { Screen } from '@components/Screen'
 import { Header } from '@components/Header'
-import { FlatList, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import { StyledPressable } from '@components/StyledPressable'
 import { useCallback, useEffect } from 'react'
 import { BackgroundIcon } from '@components/BackgroundIcon'
 import { FolderItem } from '@components/FolderItem'
 import { useTasks } from '@context/Tasks'
 import { useModal } from '@context/Modal'
+import { NoFolders } from '@components/NoFolders'
 
 export default function Index() {
   const { folders } = useTasks()
   const { openModal } = useModal()
+
+  const thereIsFolders = folders.length > 0
 
   const openCreateFolderModal = useCallback(
     (e?: any) => {
@@ -26,20 +29,19 @@ export default function Index() {
     <Screen>
       <BackgroundIcon />
       <Header />
-      <View className="w-1/4 mx-auto flex-row items-center justify-center">
-        <StyledPressable
-          text="Agregar"
-          onPress={openCreateFolderModal}
-        />
-      </View>
-      <View className="mt-10 items-center justify-center w-11/12 mx-auto flex-1 mb-6">
-        <FlatList
-          className="w-full gap-y-2"
-          data={folders}
-          renderItem={({ item }) => <FolderItem folder={item} />}
-          keyExtractor={item => item.id}
-        />
-      </View>
+
+      {!thereIsFolders && <NoFolders openModal={openCreateFolderModal} />}
+
+      {thereIsFolders && (
+        <View className="mt-10 items-center justify-center w-11/12 mx-auto flex-1 mb-6">
+          <FlatList
+            className="w-full gap-y-2"
+            data={folders}
+            renderItem={({ item }) => <FolderItem folder={item} />}
+            keyExtractor={item => item.id}
+          />
+        </View>
+      )}
     </Screen>
   )
 }
