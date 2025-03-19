87ecbbfac0317d51960294de646611c83230b458
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-18 19:18:42 -0500

    Enhance Header component: integrate animated folder visibility and add prompt for creating new folders when empty

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,51 @@
-import { Text, View } from 'react-native'
+import { Text, View, Animated, useAnimatedValue } from 'react-native'
+import { StyledPressable } from './StyledPressable'
+import { useTasks } from '@context/Tasks'
+import { useModal } from '@context/Modal'
+import { useCallback, useEffect } from 'react'
 
 export function Header() {
+  const { folders } = useTasks()
+  const { openModal } = useModal()
+  const thereIsFolders = folders.length > 0
+  const opacityValue = useAnimatedValue(thereIsFolders ? 0 : 1)
+
+  useEffect(() => {
+    const toValue = thereIsFolders ? 1 : 0
+
+    const opacityAnimation = Animated.timing(opacityValue, {
+      toValue,
+      duration: 200,
+      useNativeDriver: true,
+    })
+
+    opacityAnimation.start()
+  }, [opacityValue, thereIsFolders])
+
+  const openCreateFolderModal = useCallback(
+    (e?: any) => {
+      openModal(e, {
+        type: 'FOLDER',
+        mode: 'CREATE',
+      })
+    },
+    [openModal],
+  )
+
   return (
-    <View className="header flex-row w-full py-4 px-7 flex items-center justify-between mt-6">
-      <View className="w-full items-center justify-center">
-        <Text className="text-white text-4xl text-center">Lista de tareas</Text>
+    <View className="header flex-row w-full py-4 px-3 flex items-center justify-between mt-6">
+      <View className="items-center justify-center">
+        <Text className="text-white text-3xl text-center">Mis Carpetas</Text>
       </View>
+      <Animated.View
+        className={`max-w-32 flex-row items-center justify-center ${!thereIsFolders ? 'opacity-100' : ''}`}
+        style={{ opacity: opacityValue }}
+      >
+        <StyledPressable
+          text="Agregar"
+          onPress={openCreateFolderModal}
+        />
+      </Animated.View>
     </View>
   )
 }
