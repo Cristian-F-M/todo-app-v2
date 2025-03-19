1ca208cbe0054f086579ba19b47780d476aaa3d1
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-19 18:27:07 -0500

    Add Folder404 icon component to represent a 404 error, enhancing visual feedback for missing folders in the application

---
+++ icons/Folder404.tsx
@@ -0,0 +1,20 @@
+import Svg, { SvgProps, Path } from 'react-native-svg'
+const Folder404 = (props: SvgProps) => (
+  <Svg
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={1.5}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="lucide lucide-folder-x h-12 w-12 text-red-400"
+    {...props}
+  >
+    <Path d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z" />
+    <Path d="m9.5 10.5 5 5" />
+    <Path d="m14.5 10.5-5 5" />
+  </Svg>
+)
+export default Folder404
