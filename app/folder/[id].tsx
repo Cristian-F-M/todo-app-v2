04e0e37da452108ef1f4be85df2e8141c487efeb
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-19 13:45:05 -0500

    Refactor Folder component to use modal context for task creation and streamline task handling and state management

--- app/folder/[id].tsx
+++ app/folder/[id].tsx
@@ -1,14 +1,14 @@
 import { Screen } from '@components/Screen'
 import { useTasks } from '@context/Tasks'
 import { Stack, useGlobalSearchParams } from 'expo-router'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FlatList, Text, View } from 'react-native'
 import { StyledPressable } from '@components/StyledPressable'
-import { Modalize } from 'react-native-modalize'
 import { ScrollView } from 'react-native-gesture-handler'
 import { NoTasks } from '@components/NoTasks'
 import type { Tasks } from 'Task'
 import { TaskItem } from '@components/TaskItem'
+import { useModal } from '@context/Modal'
 
 export default function Folder() {
   const { id } = useGlobalSearchParams()
@@ -19,74 +19,74 @@ export default function Folder() {
     getTasksByFolderId,
   } = useTasks()
 
+  const { openModal } = useModal()
+
   const folderId = id instanceof Array ? id[0] : id
   const folder = getFolderById(folderId)
-  const [tasks, setTasks] = useState<Tasks>(getTasksByFolderId(folderId))
-  const modalizeRef = useRef<Modalize>(null)
+  const [tasks, setTasks] = useState<Tasks>([])
+  const [taskCount, setTaskCount] = useState<number>(folder?.taskCount || 0)
 
   const openCreateTaskModal = (e?: any) => {
-    if (e) e.persist()
-    modalizeRef.current?.open()
+    openModal(e, {
+      mode: 'CREATE',
+      type: 'TASK',
+      folderId: folderId,
+      onSubmit: () => {
+        setTaskCount(prev => prev + 1)
+      },
+    })
   }
 
   useEffect(() => {
     const tasks = getTasksByFolderId(folderId)
-    setTasks(tasks)
-  }, [tasksFromContext, getTasksByFolderId, folderId])
 
-  if (!folder) {
-    return <Text>No se encontro el folder</Text>
-  }
+    console.log({ tasks })
+
+    setTasks(tasks)
+    setTaskCount(tasks.length)
+  }, [tasksFromContext, folderId, getTasksByFolderId])
+
+  const pageTitle = folder ? folder.name : 'No encontrada'
 
   return (
     <Screen safeArea={false}>
+      {!folder && (
+        <Text className="text-white text-3xl">No se encontro el folder</Text>
+      )}
       <Stack.Screen
         options={{
           headerShown: true,
-          title: folder.name,
+          title: pageTitle,
           headerStyle: { backgroundColor: '#030712' },
           headerTintColor: '#fff',
         }}
       />
-      <View className="px-2 flex-1">
-        {/* <View className="flex-row items-center justify-start relative gap-x-2">
-          <Pressable
-            className="active:bg-gray-600 rounded p-px"
-            onPress={handleClickBack}
-          >
-            <Back
-              stroke="white"
-              width={30}
-              height={30}
+
+      {folder && (
+        <View className="px-2 flex-1">
+          <View className="flex flex-row items-center justify-between mt-3 px-2">
+            <Text className="text-gray-400 text-base">{taskCount} tareas</Text>
+            <StyledPressable
+              text="Agregar tarea"
+              pressableClassName="max-w-32"
+              onPress={openCreateTaskModal}
             />
-          </Pressable>
-          <Text className="text-white text-3xl font-bold">{folder.name}</Text>
-        </View> */}
+          </View>
 
-        <View className="flex flex-row items-center justify-between mt-3 px-2">
-          <Text className="text-gray-400 text-base">
-            {folder.taskCount} tareas
-          </Text>
-          <StyledPressable
-            text="Agregar tarea"
-            pressableClassName="max-w-32"
-            onPress={openCreateTaskModal}
-          />
+          {tasks.length > 0 && (
+            <FlatList
+              className="mt-7"
+              data={tasks}
+              renderItem={({ item }) => <TaskItem task={item} />}
+              keyExtractor={item => item.id}
+            />
+          )}
+
+          <ScrollView>
+            {tasks.length === 0 && <NoTasks openModal={openCreateTaskModal} />}
+          </ScrollView>
         </View>
-
-        {tasks.length > 0 && (
-          <FlatList
-            className="mt-7"
-            data={tasks}
-            renderItem={({ item }) => <TaskItem task={item} />}
-            keyExtractor={item => item.id}
-          />
-        )}
-
-        <ScrollView>
-          {tasks.length === 0 && <NoTasks openModal={openCreateTaskModal} />}
-        </ScrollView>
-      </View>
+      )}
     </Screen>
   )
 }
