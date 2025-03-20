f8acee7b627d2dd24d71478de0d9a4fbbad11db5
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-20 10:47:15 -0500

    Use the loader component to show feedback when folders are loading

--- app/index.tsx
+++ app/index.tsx
@@ -5,20 +5,29 @@ import { BackgroundIcon } from '@components/BackgroundIcon'
 import { FolderItem } from '@components/FolderItem'
 import { useTasks } from '@context/Tasks'
 import { NoFolders } from '@components/NoFolders'
+import { useEffect, useState } from 'react'
+import { Loader } from '@components/Loader'
 
 export default function Index() {
   const { folders } = useTasks()
+  const [loading, setLoading] = useState(true)
 
   const thereIsFolders = folders.length > 0
+
+  useEffect(() => {
+    if (folders && folders.length >= 0) setLoading(false)
+  }, [folders])
 
   return (
     <Screen>
       <BackgroundIcon />
       <Header />
 
-      {!thereIsFolders && <NoFolders />}
+      {loading && <Loader cantItems={6} />}
 
-      {thereIsFolders && (
+      {!loading && !thereIsFolders && <NoFolders />}
+
+      {!loading && thereIsFolders && (
         <View className="mt-10 items-center justify-center w-11/12 mx-auto flex-1 mb-6">
           <FlatList
             className="w-full gap-y-2"
