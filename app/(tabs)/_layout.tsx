81630b446350d31ae48bbdeed9ba717859088b3d
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-20 10:50:22 -0500

    Add layout in tabs folder and set tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,36 @@
+import Home from '@icons/Home'
+import Settings from '@icons/Settings'
+import { Tabs } from 'expo-router'
+import { View } from 'react-native'
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        animation: 'fade',
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: '#0A1E3F',
+          borderColor: '#1554bd',
+        },
+        tabBarActiveTintColor: '#0066CC',
+        tabBarInactiveTintColor: '#A2B9D9',
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Inicio',
+          tabBarIcon: ({ color }) => <Home color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="config"
+        options={{
+          title: 'Configuración',
+          tabBarIcon: ({ color }) => <Settings color={color} />,
+        }}
+      />
+    </Tabs>
+  )
+}
