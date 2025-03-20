8dd90bd953eb78d2f5f697cf081d8023f9ea4a89
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-20 18:00:54 -0500

    Add ConfigCard component for structured display of configuration settings with title and children elements

---
+++ components/ConfigCard.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from 'react-native'
+
+export function ConfigCard({
+  title,
+  children,
+}: {
+  title: string
+  children: React.ReactNode
+}) {
+  return (
+    <View className="border-b border-gray-700 py-4 px-0 mx-auto w-full">
+      <View className="mb-4">
+        <Text className="text-blue-400 text-xl font-bold">{title}</Text>
+      </View>
+      <View className="flex flex-col gap-y-4">{children}</View>
+    </View>
+  )
+}
