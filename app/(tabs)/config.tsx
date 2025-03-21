b0f607e0f6969d208b9f79d96bb7a5a6f3ebcd9e
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-20 20:04:10 -0500

    Add 'coming soon' label to ConfigCard in ConfigPage, improving user feedback on upcoming features and enhancing UI clarity

--- app/(tabs)/config.tsx
+++ app/(tabs)/config.tsx
@@ -86,6 +86,7 @@ export default function ConfigPage() {
                 setValue: setConfigs,
                 valueKey: 'clearTaskAfter',
               }}
+              commingSoon
             />
           </ConfigCard>
         </Animated.ScrollView>
