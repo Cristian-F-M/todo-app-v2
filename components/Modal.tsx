3d904787cdfc786e2f02ddc9575eebed29de58f0
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-26 11:14:13 -0500

    Enhance ModalTask component with notification options using time and date pickers, including permission management and state updates

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,7 +3,19 @@ import { Pressable, Text, TextInput, View } from 'react-native'
 import { StyledPressable } from './StyledPressable'
 import { useCallback, useEffect, useState } from 'react'
 import { useTasks } from '@context/Tasks'
-import type { ModalProps } from 'Modal'
+import type { ModalProps, NotificationTypes } from 'Modal'
+import Bell from '@icons/Bell'
+import { Switch } from 'react-native-gesture-handler'
+import type { TimeValueType, DateTimeValueType } from 'types/TimePicker'
+import { TimePicker } from './TimePicker'
+import { DateTimePicker } from './DateTimePicker'
+import { CheckboxNotificationGroup } from './CheckboxNotificationGroup'
+import { formatDateString, sumDate } from '@utils/DateTime'
+import {
+  getNotificationsPermissions,
+  sendNotification,
+} from '@utils/notifications'
+import * as Notifications from 'expo-notifications'
 
 export function ModalTask({
   openModal,
@@ -18,7 +30,17 @@ export function ModalTask({
 }: ModalProps) {
   const [textInput, setTextInput] = useState('')
   const [errors, setErrors] = useState<{ [key: string]: string | null }>({})
+  const [isNotification, setIsNotification] = useState(false)
+  const [notificationType, setNotificationType] =
+    useState<NotificationTypes>('TIME')
   const { addTask, addFolder, editFolder, editTask } = useTasks()
+  const [timeValue, setTimeValue] = useState<TimeValueType>({
+    minutes: 0,
+    hours: 0,
+  })
+  const [dateTimeValue, setDateTimeValue] = useState<DateTimeValueType>(
+    new Date(),
+  )
 
   const handleClickSubmit = () => {
     if (textInput.trim() === '') {
@@ -37,7 +59,22 @@ export function ModalTask({
     }
     if (onSubmit) onSubmit()
     closeModal()
+
+    if (isNotification) {
+      let date = dateTimeValue
+
+      if (notificationType === 'TIME') date = sumDate(new Date(), timeValue)
+
+      sendNotification(textInput, '', {
+        type: Notifications.SchedulableTriggerInputTypes.DATE,
+        date,
+      })
+    }
   }
+
+  useEffect(() => {
+    console.log({ dateTimeValue })
+  }, [dateTimeValue])
 
   useEffect(() => {
     setErrors(prev => ({ ...prev, textInput: null }))
@@ -81,6 +118,23 @@ export function ModalTask({
   const modalTitle = `${modalMode} ${modalType}`
   const pressableText = mode === 'CREATE' ? 'Agregar' : 'Guardar'
 
+  const toggleSwitch = async () => {
+    if (isNotification) {
+      await getNotificationsPermissions()
+    }
+    setNotificationType('TIME')
+    setIsNotification(previousState => !previousState)
+  }
+
+  const timeIsZero = timeValue.hours === 0 && timeValue.minutes === 0
+
+  const notificationAtText =
+    notificationType === 'TIME'
+      ? timeIsZero
+        ? 'No te notificaremos'
+        : `En ${timeValue.hours} horas y ${timeValue.minutes} minutos`
+      : `El ${formatDateString(dateTimeValue)}`
+
   return (
     <View className="relative flex-1 mx-auto w-full px-4 py-6">
       <View className="flex-row items-center justify-between relative">
@@ -115,6 +169,78 @@ export function ModalTask({
       >
         {errors.textInput}
       </Text>
+
+      <View className="bg-blue-500/20 rounded-lg mt-2">
+        <View className="flex-row items-center justify-between rounded-lg px-3 py-1">
+          <View className="flex-row items-center gap-x-2">
+            <Bell
+              color={'#60a5fa'}
+              width={24}
+              height={24}
+            />
+            <Text className="text-white text-base">Notificar</Text>
+          </View>
+          <Switch
+            trackColor={{ false: '#767577', true: '#2563eb' }}
+            thumbColor={'#fff'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isNotification}
+          />
+        </View>
+
+        {isNotification && (
+          <View>
+            <View className="flex-row items-center justify-between rounded-lg px-3 py-1">
+              <View className="w-1 h-full bg-blue-500/70 rounded"></View>
+              <View className="w-full flex-col pl-5 py-3">
+                <CheckboxNotificationGroup
+                  notificationType={notificationType}
+                  setNotificationType={setNotificationType}
+                  setDateTimeValue={setDateTimeValue}
+                  setTimeValue={setTimeValue}
+                />
+
+                <View className="mt-4">
+                  {notificationType === 'TIME' && (
+                    <TimePicker
+                      timeValue={timeValue}
+                      setTimeValue={setTimeValue}
+                    />
+                  )}
+                  {notificationType === 'DATE_TIME' && (
+                    <DateTimePicker
+                      dateTime={dateTimeValue}
+                      setDate={setDateTimeValue}
+                    />
+                  )}
+                </View>
+
+                <View className="mt-6 bg-resalt/20 px-2 py-2 rounded-md flex-row gap-x-2 items-center">
+                  <View>
+                    <Bell
+                      color={'#60a5fa'}
+                      width={17}
+                      height={17}
+                    />
+                  </View>
+                  <View className="flex-col gap-x-1">
+                    {!timeIsZero ||
+                      (notificationType === 'DATE_TIME' && (
+                        <Text className="text-gray-300">
+                          Te notificaremos:{' '}
+                        </Text>
+                      ))}
+                    <Text className="text-white text-base">
+                      {notificationAtText}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+        )}
+      </View>
 
       <View>
         <StyledPressable
