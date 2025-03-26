930a98f1f31712b8088f633e898e38169180bfde
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-25 20:45:36 -0500

    Implement notification handler in layout for better alert and sound management in the Expo app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,9 +9,18 @@ import { SQLiteProvider } from 'expo-sqlite'
 import { initDatabase } from '@utils/database'
 import * as SystemUI from 'expo-system-ui'
 import { SplashScreen } from 'expo-router'
+import * as Notifications from 'expo-notifications'
 
 SystemUI.setBackgroundColorAsync('transparent')
 SplashScreen.preventAutoHideAsync()
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+})
 
 export default function RootLayout() {
   return (
