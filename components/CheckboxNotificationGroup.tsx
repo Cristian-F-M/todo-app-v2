387a0b5cacf2df64a0caec53c6267d81b7c3a405
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-25 20:38:31 -0500

    Add CheckboxNotificationGroup component for selecting notification types with integrated date and time reset functionality

---
+++ components/CheckboxNotificationGroup.tsx
@@ -0,0 +1,90 @@
+import type { NotificationTypes } from 'Modal'
+import { useCallback } from 'react'
+import { Text, View } from 'react-native'
+import BouncyCheckbox from 'react-native-bouncy-checkbox'
+import { CheckboxIcon } from './CheckboxIcon'
+import type { TimeValueType } from 'TimePicker'
+
+export function CheckboxNotificationGroup({
+  notificationType,
+  setNotificationType,
+  setDateTimeValue,
+  setTimeValue,
+}: {
+  notificationType: NotificationTypes
+  setNotificationType: React.Dispatch<React.SetStateAction<NotificationTypes>>
+  setDateTimeValue: React.Dispatch<React.SetStateAction<Date>>
+  setTimeValue: React.Dispatch<React.SetStateAction<TimeValueType>>
+}) {
+  const resetDateTimeValue = useCallback(() => {
+    setDateTimeValue(new Date())
+  }, [setDateTimeValue])
+
+  const resetTimeValue = useCallback(() => {
+    setTimeValue({ minutes: 0, hours: 0 })
+  }, [setTimeValue])
+
+  const handleChangeNotificationType = useCallback(
+    (type: NotificationTypes) => {
+      if (type === notificationType) return
+
+      resetDateTimeValue()
+      resetTimeValue()
+      setNotificationType(type)
+    },
+    [notificationType, resetDateTimeValue, setNotificationType, resetTimeValue],
+  )
+
+  return (
+    <View className="w-full flex-col gap-2">
+      <View className="flex-row items-center gap-x-2 flex">
+        <BouncyCheckbox
+          isChecked={notificationType === 'TIME'}
+          onPress={() => {
+            handleChangeNotificationType('TIME')
+          }}
+          useBuiltInState={false}
+          size={20}
+          fillColor="#2563eb8f"
+          unFillColor="#172554"
+          disableText
+          iconComponent={
+            <CheckboxIcon isChecked={notificationType === 'TIME'} />
+          }
+        />
+        <Text
+          className="text-white"
+          onPress={() => {
+            handleChangeNotificationType('TIME')
+          }}
+        >
+          Notificar en:
+        </Text>
+      </View>
+      <View className="flex-row items-center gap-x-2">
+        <BouncyCheckbox
+          isChecked={notificationType === 'DATE_TIME'}
+          onPress={() => {
+            handleChangeNotificationType('DATE_TIME')
+          }}
+          useBuiltInState={false}
+          size={20}
+          fillColor="#2563eb8f"
+          unFillColor="#172554"
+          disableText
+          iconComponent={
+            <CheckboxIcon isChecked={notificationType === 'DATE_TIME'} />
+          }
+        />
+        <Text
+          className="text-white"
+          onPress={() => {
+            handleChangeNotificationType('DATE_TIME')
+          }}
+        >
+          Notificar fecha y hora:
+        </Text>
+      </View>
+    </View>
+  )
+}
