1b1a331b2337123791f0a4007216273555dca492
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-30 18:55:28 -0500

    Update NoTasks styles for improved theme consistency between light and dark modes in Pressable and Text components

--- components/NoTasks.tsx
+++ components/NoTasks.tsx
@@ -5,14 +5,14 @@ import { StyledPressable } from './StyledPressable'
 export function NoTasks({ openModal }: { openModal: (e?: any) => void }) {
   return (
     <View className="flex-1 items-center justify-center mt-24 w-4/5 mx-auto">
-      <Pressable className="flex-row items-center justify-center bg-gray-800 active:bg-gray-700 rounded-full p-7">
+      <Pressable className="flex-row items-center justify-center bg-gray-200 dark:bg-gray-800 active:bg-gray-100 active:dark:bg-gray-700 rounded-full p-7">
         <Folder
           width={50}
           height={50}
           stroke="#2563eb"
         />
       </Pressable>
-      <Text className="text-white text-3xl mt-3 font-semibold">
+      <Text className="dark:text-white text-3xl mt-3 font-semibold">
         No hay tasks
       </Text>
       <Text className="text-gray-500 mt-1">Esta carpeta está vacía</Text>
