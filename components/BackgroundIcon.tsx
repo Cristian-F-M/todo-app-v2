52c937e4fed5e5a4893c928ae52dc1897af863aa
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-30 17:02:51 -0500

    Add color scheme support to BackgroundIcon for improved dark mode functionality in logo display

--- components/BackgroundIcon.tsx
+++ components/BackgroundIcon.tsx
@@ -1,15 +1,19 @@
 import CMLogo from '@icons/CMLogo'
 import { View } from 'react-native'
+import { useColorScheme } from 'nativewind'
 
 export function BackgroundIcon() {
+  const { colorScheme } = useColorScheme()
+
   return (
     <View
       style={{ filter: 'blur(15px)' }}
-      className="absolute bg-black w-full h-full flex items-center justify-center backdrop-blur-3xl blur-3xl"
+      className="absolute bg-gray-300 dark:bg-black w-full h-full flex items-center justify-center backdrop-blur-3xl blur-3xl"
     >
       <CMLogo
         width={300}
         height={300}
+        color={colorScheme === 'dark' ? 'white' : 'black'}
       />
     </View>
   )
