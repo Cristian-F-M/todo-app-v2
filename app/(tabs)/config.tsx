15e66642c918d744ee19d29182e16e7336b46554
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-31 14:33:20 -0500

    Implement theme selection in ConfigPage with Picker component and persistent theme retrieval from AsyncStorage

--- app/(tabs)/config.tsx
+++ app/(tabs)/config.tsx
@@ -2,13 +2,16 @@ import { Screen } from '@components/Screen'
 import { Stack } from 'expo-router'
 import { ConfigCard } from '@components/ConfigCard'
 import { ConfigRow } from '@components/ConfigRow'
-import { useCallback, useEffect, useState } from 'react'
-import { Animated, useAnimatedValue } from 'react-native'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { Animated, Pressable, useAnimatedValue, View } from 'react-native'
 import { getAllConfigs, saveAllConfigs } from '@utils/settings'
 import { ConfigsSkeleton } from '@components/ConfigsSkeleton'
 import { useDebounce } from '@utils/useDebounce'
 import type { Configs as ConfigsType } from '@utils/settings'
 import { useColorScheme } from 'nativewind'
+import { changeTheme, THEMES } from '@utils/Theme'
+import { Picker } from '@react-native-picker/picker'
+import { getItem } from '@utils/AsyncStorage'
 
 export default function ConfigPage() {
   const [isLoading, setIsLoading] = useState(true)
@@ -43,6 +46,25 @@ export default function ConfigPage() {
     if (!isLoading) opacityAnimation.start()
   }, [isLoading, opacityAnimation])
 
+  const [selectedTheme, setSelectedTheme] =
+    useState<keyof typeof THEMES>('system')
+
+  useEffect(() => {
+    async function getTheme() {
+      const theme = await getItem({ name: 'colorScheme' })
+      setSelectedTheme(theme as keyof typeof THEMES)
+    }
+    getTheme()
+  }, [])
+
+  const pickerRef = useRef<Picker<string>>(null)
+  const handleChangeColorScheme = useCallback((itemValue: string) => {
+    changeTheme(itemValue as keyof typeof THEMES)
+    setSelectedTheme(itemValue as keyof typeof THEMES)
+  }, [])
+
+  const ThemeIcon = THEMES[selectedTheme].icon
+
   return (
     <Screen safeArea={false}>
       <Stack.Screen
@@ -62,6 +84,48 @@ export default function ConfigPage() {
           className="flex flex-col gap-y-3 w-[95%] mx-auto"
           style={{ opacity: opacityValue }}
         >
+          <ConfigCard title="Apariencia">
+            <ConfigRow
+              text={'Tema'}
+              description="Define el tema de la aplicación."
+              typeConfig="other"
+              value={{
+                value: configs,
+                setValue: setConfigs,
+                valueKey: 'colorScheme',
+              }}
+            >
+              <View className="">
+                <Pressable
+                  className="p-2 rounded-lg dark:bg-blue-600 bg-blue-400 active:dark:bg-blue-400 active:bg-blue-300 w-12 items-center justify-center self-end mr-1"
+                  onPress={() => pickerRef.current?.focus()}
+                >
+                  {
+                    <ThemeIcon
+                      color={colorScheme === 'dark' ? '#fff' : '#000'}
+                    />
+                  }
+                </Pressable>
+                <Picker
+                  style={{ display: 'none' }}
+                  className="hidden"
+                  ref={pickerRef}
+                  selectedValue={selectedTheme}
+                  onValueChange={handleChangeColorScheme}
+                  mode="dialog"
+                >
+                  {Object.entries(THEMES).map(([key, theme], index) => (
+                    <Picker.Item
+                      key={key}
+                      label={theme.label}
+                      value={theme.value}
+                    />
+                  ))}
+                </Picker>
+              </View>
+            </ConfigRow>
+          </ConfigCard>
+
           <ConfigCard title="Eliminación">
             <ConfigRow
               text={'Eliminar tarea'}
