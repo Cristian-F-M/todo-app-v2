984c62f0260c67be210785c26484e0d9989235da
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-03-30 19:51:41 -0500

    Add color scheme support to CheckboxIcon by updating background color for improved theme consistency in light and dark modes

--- components/CheckboxIcon.tsx
+++ components/CheckboxIcon.tsx
@@ -3,7 +3,7 @@ import { View } from 'react-native'
 export function CheckboxIcon({ isChecked }: { isChecked: boolean }) {
   return (
     <View
-      className={`size-10/12 bg-resalt rounded-full ${isChecked ? 'opacity-100' : 'opacity-0'}`}
+      className={`size-10/12 dark:bg-resalt bg-blue-600 rounded-full ${isChecked ? 'opacity-100' : 'opacity-0'}`}
     ></View>
   )
 }
