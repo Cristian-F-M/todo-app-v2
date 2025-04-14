1c5243020b3798d20508f25f5ea3688ccca04feb
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-14 18:40:47 -0500

    Add Toast notification before opening settings for permissions in getNotificationsPermissions, enhancing user feedback

--- utils/notifications.ts
+++ utils/notifications.ts
@@ -1,6 +1,6 @@
 import * as Notifications from 'expo-notifications'
 import type { PermissionStatus } from 'expo-notifications'
-import { Linking } from 'react-native'
+import { Linking, ToastAndroid } from 'react-native'
 
 type SendNotificationFC = (
   title: string,
@@ -14,11 +14,15 @@ const DEFAULT_NOTIFICATIONS_TRIGGER: Notifications.SchedulableNotificationTrigge
     date: new Date(),
   }
 
+let openSettingsTimeout: null | NodeJS.Timeout = null
+
 export async function getNotificationsPermissions(): Promise<{
   status: PermissionStatus
   canAskAgain: boolean
   granted: boolean
 }> {
+  if (openSettingsTimeout) clearTimeout(openSettingsTimeout)
+
   const { status, canAskAgain, granted } =
     await Notifications.getPermissionsAsync()
 
@@ -27,12 +31,15 @@ export async function getNotificationsPermissions(): Promise<{
   }
 
   if (!granted && canAskAgain) {
-    Notifications.requestPermissionsAsync()
-    return getNotificationsPermissions()
+    const permissionResponse = await Notifications.requestPermissionsAsync()
+    return permissionResponse
   }
 
   if (!granted && !canAskAgain) {
-    Linking.openSettings()
+    ToastAndroid.show('Abriendo las configuraciones', ToastAndroid.SHORT)
+    openSettingsTimeout = setTimeout(() => {
+      Linking.openSettings()
+    }, 2000)
   }
 
   return { status, canAskAgain, granted }
