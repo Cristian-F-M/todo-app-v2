10fecd1b14dfae62733d0f1a51575f2191c43d8f
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-07-01 15:15:29 -0500

    Add StatusBar component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,8 @@ import { NoFolders } from '@components/NoFolders'
 import { useEffect, useState } from 'react'
 import { Loader } from '@components/Loader'
 import { SplashScreen } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { useColorScheme } from 'nativewind'
 
 export default function Index() {
   const { folders } = useTasks()
@@ -37,11 +39,20 @@ export default function Index() {
     }
   }, [opacityValue])
 
+  const { colorScheme } = useColorScheme()
+  const themeStyle = colorScheme === 'dark' ? 'light' : 'dark'
+
   return (
     <Screen
       safeArea={false}
       style={{ opacity: opacityValue }}
     >
+      <StatusBar
+        translucent={false}
+        style={themeStyle}
+        backgroundColor={'transparent'}
+      />
+
       <BackgroundIcon />
       <Header />
 
