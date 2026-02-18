39c8035d9663c167c53ea7f9008e15a664d876a8
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-18 13:09:05 -0500

    Remove unnecessary functions and use news

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,19 +1,11 @@
 import * as SQLite from 'expo-sqlite'
-
-export function connectDatabase() {
-	const db = SQLite.openDatabaseSync('todo-cm.db', {
-		useNewConnection: true
-	})
-	return db
-}
+import { executeQuery } from '@/database/querys'
 
 export async function initDatabase() {
-	const db = connectDatabase()
-
-	await db.execAsync(
+	executeQuery(
 		'CREATE TABLE IF NOT EXISTS folders (id TEXT PRIMARY KEY, name TEXT, taskCount INTEGER)'
 	)
-	await db.execAsync(
+	executeQuery(
 		'CREATE TABLE IF NOT EXISTS tasks (id TEXT PRIMARY KEY, name TEXT, folderId TEXT, notificationId TEXT, FOREIGN KEY(folderId) REFERENCES folders(id))'
 	)
 }
@@ -21,17 +13,13 @@ export async function initDatabase() {
 export async function removeNotificationId(notificationId: string) {
 	if (!notificationId) return
 
-	const db = connectDatabase()
-	const statement = await db.prepareAsync(
-		'UPDATE tasks SET notificationId = null WHERE notificationId = $notificationId'
+	const { succes, message } = executeQuery(
+		'UPDATE tasks SET notificationId = null WHERE notificationId = ?',
+		[notificationId]
 	)
-	let ok = false
+	return { ok: succes, message }
+}
 
-	try {
-		await statement.executeAsync({ $notificationId: notificationId })
-		ok = true
-	} finally {
-		await statement.finalizeAsync()
-	}
-	return { ok }
+export function migrateDB() {
+	
 }
