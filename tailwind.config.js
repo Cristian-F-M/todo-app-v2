643de3a9375ddf8ab7209d93070f49923c16b773
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-17 21:45:01 -0500

    Fix path for nativewind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   darkMode: 'class',
   content: [
     './app/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-    './icons/**/*.{js,jsx,ts,tsx}',
-    './app/index.tsx',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/icons/**/*.{js,jsx,ts,tsx}',
+    './src/app/index.tsx',
   ],
   presets: [require('nativewind/preset')],
   theme: {
