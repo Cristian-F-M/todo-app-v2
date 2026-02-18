c20e5080cd3a056ad01ac15e35096c63d8328e34
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-18 12:13:24 -0500

    Add database query functions for folder and task

---
+++ src/database/querys/folder.ts
@@ -0,0 +1,74 @@
+import { ToastAndroid } from 'react-native'
+import { executeQuery, select } from '@/database/querys'
+import type { Folder } from '@/types/Folder'
+
+export function getAll(): Folder[] {
+	const { succes, result, message } = select<Folder[]>(
+		'SELECT * FROM folders',
+		{
+			all: true
+		}
+	)
+
+	if (!succes || !result) {
+		const msg = message || 'No se pudieron obtener las carpetas'
+		ToastAndroid.show(msg, ToastAndroid.LONG)
+		return []
+	}
+
+	return result
+}
+
+export function getById(id: string) {
+	const { succes, result, message } = select<Folder>(
+		'SELECT * FROM folders WHERE id = ?',
+		{
+			all: false,
+			params: [id]
+		}
+	)
+
+	if (!succes || !result) {
+		const msg = message || 'No se pudo obtener la carpeta'
+		ToastAndroid.show(msg, ToastAndroid.LONG)
+		return null
+	}
+
+	return result
+}
+
+export function create(folder: Folder) {
+	const { succes, message, result } = executeQuery(
+		'INSERT INTO folders (id, name, taskCount) VALUES (?, ?, ?)',
+		[folder.id, folder.name, folder.taskCount]
+	)
+	if (!succes || !result) {
+		const msg = message || 'No se pudo crear la carpeta'
+		ToastAndroid.show(msg, ToastAndroid.LONG)
+	}
+	return succes
+}
+
+export function update(folder: Folder) {
+	const { succes, message, result } = executeQuery(
+		'UPDATE folders SET name = ?, taskCount = ? WHERE id = ?',
+		[folder.name, folder.taskCount, folder.id]
+	)
+	if (!succes || !result) {
+		const msg = message || 'No se pudo actualizar la carpeta'
+		ToastAndroid.show(msg, ToastAndroid.LONG)
+	}
+	return succes
+}
+
+export function deleteById(id: string) {
+	const { succes, message, result } = executeQuery(
+		'DELETE FROM folders WHERE id = ?',
+		[id]
+	)
+	if (!succes || !result) {
+		const msg = message || 'No se pudo eliminar la carpeta'
+		ToastAndroid.show(msg, ToastAndroid.LONG)
+	}
+	return succes
+}
---
+++ src/database/querys/task.ts
@@ -0,0 +1,87 @@
+import { ToastAndroid } from 'react-native'
+import { executeQuery, select } from '@/database/querys'
+import type { Task } from '@/types/Task'
+
+export function getAll() {
+	const { succes, result, message } = select<Task>('SELECT * FROM tasks', {
+		all: true
+	})
+
+	if (!succes || !result) {
+		const msg = message || 'No se pudieron obtener las tareas'
+		ToastAndroid.show(msg, ToastAndroid.LONG)
+		return []
+	}
+
+	return result
+}
+
+export function getById(id: string) {
+	const { succes, result, message } = select<Task>(
+		'SELECT * FROM tasks WHERE id = ?',
+		{
+			all: false,
+			params: [id]
+		}
+	)
+
+	if (!succes || !result) {
+		const msg = message || 'No se pudo obtener la tarea'
+		ToastAndroid.show(msg, ToastAndroid.LONG)
+		return null
+	}
+
+	return result
+}
+
+export function create(task: Task) {
+	const { succes, message, result } = executeQuery(
+		'INSERT INTO tasks (id, name, folderId) VALUES (?, ?, ?)',
+		[task.id, task.name, task.folderId]
+	)
+
+	if (!succes || !result) {
+		const msg = message || 'No se pudo crear la tarea'
+		ToastAndroid.show(msg, ToastAndroid.LONG)
+	}
+	return succes
+}
+
+export function update(task: Task) {
+	const { succes, message, result } = executeQuery(
+		'UPDATE tasks SET name = ?, folderId = ? WHERE id = ?',
+		[task.name, task.folderId, task.id]
+	)
+
+	if (!succes || !result) {
+		const msg = message || 'No se pudo actualizar la tarea'
+		ToastAndroid.show(msg, ToastAndroid.LONG)
+	}
+	return succes
+}
+
+export function deleteById(id: string) {
+	const { succes, message, result } = executeQuery(
+		'DELETE FROM tasks WHERE id = ?',
+		[id]
+	)
+
+	if (!succes || !result) {
+		const msg = message || 'No se pudo eliminar la tarea'
+		ToastAndroid.show(msg, ToastAndroid.LONG)
+	}
+	return succes
+}
+
+export function deleteByFolderId(folderId: string) {
+	const { succes, message, result } = executeQuery(
+		'DELETE FROM tasks WHERE folderId = ?',
+		[folderId]
+	)
+
+	if (!succes || !result) {
+		const msg = message || 'No se pudieron eliminar las tareas'
+		ToastAndroid.show(msg, ToastAndroid.LONG)
+	}
+	return succes
+}
