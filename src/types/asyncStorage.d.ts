41d1a4372438f3985fbeb0fef707990700dc3fa0
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-21 16:36:59 -0500

    Add comment line to ignore a linter rule

--- src/types/asyncStorage.d.ts
+++ src/types/asyncStorage.d.ts
@@ -1,4 +1,5 @@
 export type KeyItem = string
+// biome-ignore lint/suspicious/noExplicitAny: It does not matter
 export type ValueItem = string | number | Record<string, any> | any[] | boolean
 
 export type RemoveItemProps = {
