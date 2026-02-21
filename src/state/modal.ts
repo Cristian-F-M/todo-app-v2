86ecf590995d6e103d3deb31d9e36ca596fb9ae1
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-21 10:33:24 -0500

    Use new modal types in global state

--- src/state/modal.ts
+++ src/state/modal.ts
@@ -1,40 +1,13 @@
 import { create } from 'zustand'
-import type { Folder } from '@/types/Folder'
-import type { Task } from '@/types/Task'
-
-type Item =
-	| { type: 'FOLDER'; data: Folder }
-	| { type: 'TASK'; data: Task }
-	| null
-
-interface Modal {
-	open: (() => void) | null
-	close: (() => void) | null
-	isOpen: boolean
-}
-
-interface Modals {
-	folder: Modal
-	task: Modal
-	delete: Modal
-}
+import type { ModalFn, ModalItem, Modals, SetModalFn } from '@/types/modal'
 
 interface ModalState {
-	item: Item
+	item: ModalItem
 	modals: Modals
-	openModal: (key: keyof Modals) => void
-	closeModal: (key: keyof Modals) => void
-	setModal: (
-		key: keyof Modals,
-		{
-			open,
-			close
-		}: {
-			open: () => void
-			close: () => void
-		}
-	) => void
-	setItem: (item: Item) => void
+	openModal: ModalFn
+	closeModal: ModalFn
+	setModal: SetModalFn
+	setItem: (item: ModalItem) => void
 	folderId: string | null
 	setFolderId: (folderId: string | null) => void
 }
@@ -59,31 +32,22 @@ export const useModal = create<ModalState>()((set, get) => ({
 	},
 	item: null,
 	folderId: null,
-	setFolderId: (folderId: string | null) => set({ folderId }),
-	setModal: (
-		key: keyof Modals,
-		{
-			open,
-			close
-		}: {
-			open: () => void
-			close: () => void
-		}
-	) => {
+	setFolderId: (folderId) => set({ folderId }),
+	setModal: (key, { open, close }) => {
 		const { modals } = get()
 		const modal = modals[key]
 
 		set({ modals: { ...modals, [key]: { ...modal, open, close } } })
 	},
-	setItem: (item: Item) => set({ item }),
-	openModal: (key: keyof Modals) => {
+	setItem: (item) => set({ item }),
+	openModal: (key) => {
 		const { modals } = get()
 		const modal = modals[key]
 		modal.open?.()
 
 		set({ modals: { ...modals, [key]: { ...modal, isOpen: true } } })
 	},
-	closeModal: (key: keyof Modals) => {
+	closeModal: (key) => {
 		const { modals } = get()
 		const modal = modals[key]
 		modal.close?.()
