ba43ad49e4675ab4406eeb1ee4520e229ee7f1ad
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-21 10:26:29 -0500

    Add modal to main layout and save their functions in global state

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,13 +7,30 @@ import * as Notifications from 'expo-notifications'
 import * as SplashScreen from 'expo-splash-screen'
 import { SQLiteProvider } from 'expo-sqlite'
 import * as SystemUI from 'expo-system-ui'
-import { useEffect, useLayoutEffect } from 'react'
+import { useCallback, useEffect, useLayoutEffect, useRef } from 'react'
+import type { Modalize } from 'react-native-modalize'
 import { Host } from 'react-native-portalize'
+import {
+	configureReanimatedLogger,
+	ReanimatedLogLevel
+} from 'react-native-reanimated'
+import { DeleteModal } from '@/components/DeleteModal'
+import { FolderModal } from '@/components/FolderModal'
+import { Modal } from '@/components/Modal'
+import { Screen } from '@/components/Screen'
+import { TaskModal } from '@/components/TaskModal'
 import { useConfig } from '@/state/config'
 import useFolder from '@/state/Folder'
+import { useModal } from '@/state/modal'
 import useTask from '@/state/Task'
 import { useTheme } from '@/state/theme'
 import { migrateDB, removeNotificationId } from '@/utils/database'
+
+// This is the default configuration
+configureReanimatedLogger({
+	level: ReanimatedLogLevel.warn,
+	strict: false
+})
 
 SystemUI.setBackgroundColorAsync('transparent')
 SplashScreen.preventAutoHideAsync()
@@ -35,6 +52,10 @@ export default function RootLayout() {
 	const themeStyle = theme === 'dark' ? 'light' : 'dark'
 	const { load: loadFolders } = useFolder()
 	const { load: loadTasks } = useTask()
+	const { modals, setModal, openModal } = useModal()
+	const taskModalRef = useRef<Modalize>(null)
+	const folderModalRef = useRef<Modalize>(null)
+	const deleteModalRef = useRef<Modalize>(null)
 
 	useLayoutEffect(() => {
 		useTheme.getState().load()
@@ -49,6 +70,19 @@ export default function RootLayout() {
 		init()
 	}, [loadFolders, loadTasks, loadConfigs])
 
+	const getModalFns = useCallback((ref: React.RefObject<Modalize | null>) => {
+		return {
+			open: () => ref.current?.open(),
+			close: () => ref.current?.close()
+		}
+	}, [])
+
+	useLayoutEffect(() => {
+		setModal('task', { ...getModalFns(taskModalRef) })
+		setModal('folder', { ...getModalFns(folderModalRef) })
+		setModal('delete', { ...getModalFns(deleteModalRef) })
+	}, [setModal, getModalFns])
+
 	return (
 		<GestureHandlerRootView>
 			<SQLiteProvider databaseName="todo-cm.db" onInit={migrateDB}>
@@ -62,6 +96,18 @@ export default function RootLayout() {
 							}}
 						/>
 					</View>
+
+					<Modal modalRef={taskModalRef}>
+						<TaskModal />
+					</Modal>
+
+					<Modal modalRef={folderModalRef}>
+						<FolderModal handleClose={() => {}} />
+					</Modal>
+
+					<Modal modalRef={deleteModalRef}>
+						<DeleteModal />
+					</Modal>
 				</Host>
 			</SQLiteProvider>
 		</GestureHandlerRootView>
