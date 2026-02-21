2a27c600ec01d0f5e509e24751c9134456c91bb9
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-21 14:49:33 -0500

    Change theme config to use modal and WheelPicker

--- app/(tabs)/config.tsx
+++ app/(tabs)/config.tsx
@@ -9,6 +9,7 @@ import {
 } from 'react'
 import { Pressable, ScrollView, Text, View } from 'react-native'
 import type { Modalize } from 'react-native-modalize'
+import { twMerge } from 'tailwind-merge'
 import { ConfigCard } from '@/components/ConfigCard'
 import { ConfigRow } from '@/components/ConfigRow'
 import { Modal } from '@/components/Modal'
@@ -126,11 +127,44 @@ export default function ConfigPage() {
 			</ScrollView>
 
 			<Modal modalRef={modalRef}>
-				<View className="flex flex-col items-center justify-center py-4 px-6 gap-y-3">
+				<View className="flex flex-col items-center justify-center py-6 px-6 gap-y-3">
 					<Text className="dark:text-gray-300 text-gray-700 text-2xl font-bold mb-5">
 						Selecciona un tema
 					</Text>
-					<View className="w-44">
+
+					<View className="flex flex-row gap-x-3">
+						{Object.values(THEMES).map((t) => {
+							const Icon = t.icon
+							const isThisSelected = theme === t.value
+							let textColor = colorScheme === 'dark' ? '#777' : '#333'
+
+							if (isThisSelected)
+								textColor = colorScheme === 'dark' ? '#3b82f6' : '#1e3a8a'
+
+							return (
+								<Pressable
+									onPress={() =>
+										handleChangeColorScheme(t.value as ThemeString)
+									}
+									key={t.value}
+									className={twMerge(
+										'flex-1 flex-col items-center gap-y-1 dark:bg-[#16222e] bg-gray-400/30 p-6 rounded-xl border-2 dark:border-blue-900/10 border-gray-400/40',
+										isThisSelected &&
+											'bg-blue-300/40 border-blue-500/60 dark:bg-[#17314a] dark:border-blue-500'
+									)}
+								>
+									<Icon color={textColor} width={24} height={24} />
+									<Text
+										className="text-white text-sm"
+										style={{ color: textColor }}
+									>
+										{t.label}
+									</Text>
+								</Pressable>
+							)
+						})}
+					</View>
+					<View className="w-60 mt-5">
 						<WheelPicker
 							items={Object.values(THEMES).map((theme) => ({
 								label: theme.label,
@@ -138,7 +172,7 @@ export default function ConfigPage() {
 							}))}
 							selectedValue={theme}
 							onValueChange={(theme: string) => {
-								handleChangeColorScheme(theme as ThemeString)
+								setTheme(theme as ThemeString)
 							}}
 						/>
 					</View>
