2187da817647104cd8edf50ec804d1abad3bdec1
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-21 15:19:18 -0500

    Use ConfigRow with type as modal

--- app/(tabs)/config.tsx
+++ app/(tabs)/config.tsx
@@ -21,6 +21,7 @@ import type { ThemeString } from '@/types/theme'
 import { type Configs as ConfigsType, saveAllConfigs } from '@/utils/settings'
 import { THEMES } from '@/utils/theme'
 import { useDebounce } from '@/utils/useDebounce'
+import { ChangeThemeModalContent } from '@/components/ChangeThemeModalContent'
 
 export default function ConfigPage() {
 	const { configs, setConfigs } = useConfig()
@@ -77,7 +78,9 @@ export default function ConfigPage() {
 					<ConfigRow
 						text={'Tema'}
 						description="Define el tema de la aplicación."
-						typeConfig="other"
+						typeConfig="modal"
+						modalContent={<ChangeThemeModalContent />}
+						modalRef={modalRef}
 					>
 						<View className="">
 							<Pressable
@@ -125,59 +128,6 @@ export default function ConfigPage() {
 					/>
 				</ConfigCard>
 			</ScrollView>
-
-			<Modal modalRef={modalRef}>
-				<View className="flex flex-col items-center justify-center py-6 px-6 gap-y-3">
-					<Text className="dark:text-gray-300 text-gray-700 text-2xl font-bold mb-5">
-						Selecciona un tema
-					</Text>
-
-					<View className="flex flex-row gap-x-3">
-						{Object.values(THEMES).map((t) => {
-							const Icon = t.icon
-							const isThisSelected = theme === t.value
-							let textColor = colorScheme === 'dark' ? '#777' : '#333'
-
-							if (isThisSelected)
-								textColor = colorScheme === 'dark' ? '#3b82f6' : '#1e3a8a'
-
-							return (
-								<Pressable
-									onPress={() =>
-										handleChangeColorScheme(t.value as ThemeString)
-									}
-									key={t.value}
-									className={twMerge(
-										'flex-1 flex-col items-center gap-y-1 dark:bg-[#16222e] bg-gray-400/30 p-6 rounded-xl border-2 dark:border-blue-900/10 border-gray-400/40',
-										isThisSelected &&
-											'bg-blue-300/40 border-blue-500/60 dark:bg-[#17314a] dark:border-blue-500'
-									)}
-								>
-									<Icon color={textColor} width={24} height={24} />
-									<Text
-										className="text-white text-sm"
-										style={{ color: textColor }}
-									>
-										{t.label}
-									</Text>
-								</Pressable>
-							)
-						})}
-					</View>
-					<View className="w-60 mt-5">
-						<WheelPicker
-							items={Object.values(THEMES).map((theme) => ({
-								label: theme.label,
-								value: theme.value
-							}))}
-							selectedValue={theme}
-							onValueChange={(theme: string) => {
-								setTheme(theme as ThemeString)
-							}}
-						/>
-					</View>
-				</View>
-			</Modal>
 		</Screen>
 	)
 }
