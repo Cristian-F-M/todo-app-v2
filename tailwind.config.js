9755a81e79ccf82231da3f5e957279dd69fdd6a1
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-23 17:08:44 -0500

    Format file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  darkMode: 'class',
-  content: [
-    './app/**/*.{js,jsx,ts,tsx}',
-    './src/components/**/*.{js,jsx,ts,tsx}',
-    './src/icons/**/*.{js,jsx,ts,tsx}',
-    './src/app/index.tsx',
-  ],
-  presets: [require('nativewind/preset')],
-  theme: {
-    extend: {
-      colors: {
+	// NOTE: Update this to include the paths to all of your component files.
+	darkMode: 'class',
+	content: [
+		'./app/**/*.{js,jsx,ts,tsx}',
+		'./src/components/**/*.{js,jsx,ts,tsx}',
+		'./src/icons/**/*.{js,jsx,ts,tsx}',
+		'./src/app/index.tsx',
+	],
+	presets: [require('nativewind/preset')],
+	theme: {
+		extend: {
+			colors: {
 				background: 'var(--background)',
 				surface: 'var(--surface)',
 				surfaceSoft: 'var(--surfaceSoft)',
@@ -30,7 +30,7 @@ module.exports = {
 
 				overlay: 'var(--overlay)',
 			}
-    },
-  },
-  plugins: [],
+		},
+	},
+	plugins: [],
 }
