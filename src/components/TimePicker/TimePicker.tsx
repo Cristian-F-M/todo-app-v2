feabbcd867a586d50b86627554b7f12406b074fb
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-22 19:19:44 -0500

    Add logic to show selected time picker

--- src/components/TimePicker/TimePicker.tsx
+++ src/components/TimePicker/TimePicker.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 import { Animated, useAnimatedValue, View } from 'react-native'
 import { SingleTimePicker } from '@/components/TimePicker/SingleTimePicker'
+import { WheelPicker } from '@/components/WheelPicker/WheelPicker'
+import { useConfig } from '@/state/config'
 import type { TimeValueType } from '@/types/timePicker'
 
 export interface TimePickerProps {
@@ -10,7 +12,7 @@ export interface TimePickerProps {
 
 export function TimePicker({ value, onChange }: TimePickerProps) {
 	const animatedValue = useAnimatedValue(0)
-
+	const { configs } = useConfig()
 	useEffect(() => {
 		const animation = Animated.timing(animatedValue, {
 			toValue: 1,
@@ -28,20 +30,52 @@ export function TimePicker({ value, onChange }: TimePickerProps) {
 			className="flex flex-row w-full"
 			style={{ opacity: animatedValue }}
 		>
-			<SingleTimePicker
-				text="Horas"
-				value={value.hours}
-				onChange={(hours) => onChange({ ...value, hours })}
-				min={0}
-			/>
-			<View className="w-1 h-full dark:bg-resalt/30 bg-blue-600 rounded mx-3" />
-			<SingleTimePicker
-				text="Minutos"
-				value={value.minutes}
-				onChange={(minutes) => onChange({ ...value, minutes })}
-				min={0}
-				max={59}
-			/>
+			{configs.timePickerType === 'WHEEL' && (
+				<>
+					<View className="w-28">
+						<WheelPicker
+							items={Array.from({ length: 24 }, (_, i) => ({
+								label: String(i),
+								value: String(i)
+							}))}
+							itemHeight={30}
+							selectedValue={value.hours}
+							onValueChange={(v) => onChange({ ...value, hours: Number(v) })}
+						/>
+					</View>
+
+					<View className="w-28">
+						<WheelPicker
+							items={Array.from({ length: 60 }, (_, i) => ({
+								label: String(i),
+								value: String(i)
+							}))}
+							itemHeight={30}
+							selectedValue={value.minutes}
+							onValueChange={(v) => onChange({ ...value, minutes: Number(v) })}
+						/>
+					</View>
+				</>
+			)}
+
+			{configs.timePickerType === 'CLASIC' && (
+				<>
+					<SingleTimePicker
+						text="Horas"
+						value={value.hours}
+						onChange={(hours) => onChange({ ...value, hours })}
+						min={0}
+					/>
+					<View className="w-1 h-full dark:bg-resalt/30 bg-blue-600 rounded mx-3" />
+					<SingleTimePicker
+						text="Minutos"
+						value={value.minutes}
+						onChange={(minutes) => onChange({ ...value, minutes })}
+						min={0}
+						max={59}
+					/>
+				</>
+			)}
 		</Animated.View>
 	)
 }
