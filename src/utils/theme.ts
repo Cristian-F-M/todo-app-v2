9843023af10e2e4ccc7a0f5aba3b441f08a3e1da
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-24 18:51:59 -0500

    Add function to get theme color and custom hook to re-render once thime render components (like header, tabsBar, ...)

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,8 +1,22 @@
+import { useMemo } from 'react'
 import { THEMES } from '@/constants/theme'
 import { useTheme } from '@/state/theme'
 import type { Theme } from '@/types/theme'
 
-export function getThemeColor(color: keyof (typeof THEMES)[Theme]) {
+export function getThemeColor(
+	color: keyof (typeof THEMES)[Theme],
+	alpha?: number
+) {
 	const { theme } = useTheme.getState()
-	return `rgb(${THEMES[theme][color]})`
+	return `rgba(${THEMES[theme][color]} / ${alpha ?? 1})`
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: -
+export function useThemeStyles<T = any>(fn: () => T) {
+	const { theme } = useTheme()
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: -
+	const memoStyles = useMemo(() => fn(), [fn, theme])
+
+	return memoStyles
 }
