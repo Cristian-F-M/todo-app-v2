1396775427180d339c301a61d751234c97201bd9
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-24 18:52:12 -0500

    Use theme colors in background icon

--- src/components/layout/BackgroundIcon.tsx
+++ src/components/layout/BackgroundIcon.tsx
@@ -1,22 +1,27 @@
 import { View } from 'react-native'
 import CMLogo from '@/icons/CMLogo'
-import { getThemeColor } from '@/utils/theme'
+import { getThemeColor, useThemeStyles } from '@/utils/theme'
 
 export function BackgroundIcon() {
+	const [backgroundColor, iconColor] = useThemeStyles(() => [
+		getThemeColor('background'),
+		getThemeColor('primary', 0.4)
+	])
+
 	return (
 		<View
 			// TODO: Remove backdrop-blur-3xl blur-3xl class names
-			style={{
-				backgroundColor: getThemeColor('background'),
-				filter: 'blur(15px)'
-			}}
+			style={{ backgroundColor }}
 			className="absolute w-full h-full flex items-center justify-center"
 		>
 			<CMLogo
+				style={{
+					filter: 'blur(15px)'
+				}}
 				// TODO: Change size to 200
 				width={300}
 				height={300}
-				color={getThemeColor('primary', 0.4)}
+				color={iconColor}
 			/>
 		</View>
 	)
