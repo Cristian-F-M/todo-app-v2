cac11155a6522e00b1187f2160bf68170dda97b4
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-25 14:55:33 -0500

    Remove list of themes and add Modal config for configRow component

--- src/components/theme/ChangeThemeModalContent.tsx
+++ src/components/theme/ChangeThemeModalContent.tsx
@@ -1,23 +1,40 @@
-import { useMemo } from 'react'
-import { FlatList, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import uuid from 'react-native-uuid'
 import { THEMES } from '@/constants/theme'
 import { useTheme } from '@/state/theme'
-import type { Theme } from '@/types/theme'
+import type { ConfigRowModalWithListProps } from '@/types/config'
+import type { Theme, ThemeObject } from '@/types/theme'
 import { getThemeColor } from '@/utils/theme'
 import { ThemeOverview } from './ThemeOverview'
 
+const themeKeys = Object.values(THEMES).map(() => uuid.v4())
+
+export const ConfigModalConfig = {
+	flatList: true,
+	flatListProps: {
+		data: Object.entries(THEMES),
+		renderItem: ({ item }: { item: [string, ThemeObject] }) => (
+			<ThemeOverview
+				theme={item[1]}
+				themeKey={item[0] as Theme}
+				isSelected={item[0] === useTheme.getState().theme}
+			/>
+		),
+		keyExtractor: (_: unknown, index: number) => themeKeys[index],
+		ListHeaderComponent: () => <ChangeThemeModalContent />,
+
+		initialNumToRender: 6,
+		maxToRenderPerBatch: 4,
+		windowSize: 3,
+		removeClippedSubviews: true
+	}
+} satisfies ConfigRowModalWithListProps
+
 export function ChangeThemeModalContent() {
 	const { theme } = useTheme()
-	const themeKeys = useMemo(() => {
-		return Object.values(THEMES).map(() => uuid.v4())
-	}, [])
 
 	return (
-		<View
-			className="flex flex-col justify-center py-6 px-6"
-			style={{ maxHeight: 600 }}
-		>
+		<View className="flex flex-col w-full" style={{ maxHeight: 400 }}>
 			<Text
 				className="text-2xl font-bold mb-5"
 				style={{ color: getThemeColor('text-primary') }}
@@ -43,21 +60,6 @@ export function ChangeThemeModalContent() {
 			>
 				Temas disponibles
 			</Text>
-
-			<FlatList
-				className="mb-4"
-				data={Object.entries(THEMES)}
-				renderItem={({ item }) => (
-					<ThemeOverview
-						theme={item[1]}
-						themeKey={item[0] as Theme}
-						isSelected={item[0] === theme}
-					/>
-				)}
-				keyExtractor={(_, index) => themeKeys[index]}
-				maxToRenderPerBatch={1}
-				updateCellsBatchingPeriod={1}
-			/>
 		</View>
 	)
 }
