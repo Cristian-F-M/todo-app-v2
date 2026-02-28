b10e077d1ac4845d64aacfed233a5bd356964d91
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-28 16:43:47 -0500

    Refactor modal to simplify logic

--- src/components/modal/ColorPicker.tsx
+++ src/components/modal/ColorPicker.tsx
@@ -1,53 +1,29 @@
-import { useCallback, useMemo, useState } from 'react'
-import { View } from 'react-native'
+import { IconAlertTriangle } from '@tabler/icons-react-native'
+import { Text, View } from 'react-native'
 import type { Modalize } from 'react-native-modalize'
 import ColorPicker, {
 	type ColorFormatsObject,
 	type ColorPickerProps
 } from 'reanimated-color-picker'
 import { getThemeColor } from '@/utils/theme'
-import { StyledPressable } from '../layout/StyledPressable'
 import { Modal } from './Modal'
 
 interface ColorPickerModalProps extends ColorPickerProps {
 	value: string
+	defaultValue?: string
 	modalRef: React.RefObject<Modalize | null>
-	onColorChange?: (color: ColorFormatsObject) => void
+	onValueChange?: (color: ColorFormatsObject) => void
 	children: React.ReactNode
 }
 
 export function ColorPickerModal({
 	value,
+	defaultValue,
 	modalRef,
-	onColorChange,
-	children
+	onValueChange,
+	children,
+	...props
 }: ColorPickerModalProps) {
-	const [wasColorSelected, setWasColorSelected] = useState(false)
-	const defaultColor = useMemo(() => value ?? '#fff', [value])
-	const [selectedColor, setSelectedColor] = useState(value ?? defaultColor)
-
-	const handleCloseModal = useCallback(() => {
-		modalRef.current?.close()
-		setWasColorSelected(true)
-	}, [modalRef])
-
-	const handleOnCloseModal = useCallback(() => {
-		if (!wasColorSelected) setSelectedColor(defaultColor)
-	}, [defaultColor, wasColorSelected])
-
-	const handleCompleteColor = useCallback(
-		(color: ColorFormatsObject) => {
-			setSelectedColor(color.hex)
-			if (onColorChange) onColorChange(color)
-		},
-		[onColorChange]
-	)
-
-	const handleSelectColor = useCallback(() => {
-		handleCloseModal()
-		setWasColorSelected(true)
-	}, [handleCloseModal])
-
 	return (
 		<Modal
 			modalRef={modalRef}
@@ -55,28 +31,45 @@ export function ColorPickerModal({
 				paddingVertical: 20,
 				paddingHorizontal: 20
 			}}
-			onClose={handleOnCloseModal}
 		>
 			<View
-				className="items-center p-4 rounded-lg"
+				className="items-center p-4 rounded-lg mb-6"
 				style={{
 					backgroundColor: getThemeColor('surface-soft')
 				}}
 			>
 				<ColorPicker
 					style={{ width: '100%', gap: 10 }}
-					value={selectedColor}
-					onCompleteJS={handleCompleteColor}
+					value={value}
+					onCompleteJS={(color) => {
+						onValueChange?.(color)
+					}}
+					{...props}
 				>
 					{children}
 				</ColorPicker>
-			</View>
 
-			<StyledPressable
-				onPress={handleSelectColor}
-				className="mt-6 mb-6"
-				text="Seleccionar"
-			/>
+				<View className="flex-row px-3 gap-x-1">
+					<IconAlertTriangle
+						style={{
+							marginTop: 5
+						}}
+						size={14}
+						color={getThemeColor('warning')}
+					/>
+					<Text
+						className="text-sm text-center mt-1"
+						style={{
+							color: getThemeColor('text-secondary')
+						}}
+					>
+						El color se selecciona automáticamente. Puedes cerrar cuando
+						quieras.
+					</Text>
+				</View>
+
+				<View></View>
+			</View>
 		</Modal>
 	)
 }
