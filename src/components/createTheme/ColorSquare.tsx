a3afea9bd3edeb1aab8db989305fbe911a9bb517
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-27 19:52:30 -0500

    Allow show a theme color and change style if color can be edited

---
+++ src/components/createTheme/ColorSquare.tsx
@@ -0,0 +1,61 @@
+import {
+	Pressable,
+	type PressableProps,
+	type StyleProp,
+	View,
+	type ViewStyle
+} from 'react-native'
+import { twMerge } from 'tailwind-merge'
+import { getThemeColor } from '@/utils/theme'
+
+interface ColorSquareProps extends PressableProps {
+	editable?: boolean
+	value: string
+	style?: StyleProp<ViewStyle>
+}
+
+export function ColorSquare({
+	className,
+	style,
+	editable = false,
+	value,
+	...props
+}: ColorSquareProps) {
+	const pressableEditableStyles: StyleProp<ViewStyle> = {
+		borderWidth: 1,
+		borderStyle: 'dashed',
+		borderColor: getThemeColor('text-secondary'),
+		backgroundColor: 'transparent'
+	}
+
+	const pressableEditableClassNames =
+		'p-1 rounded-md items-center justify-center'
+
+	return (
+		<Pressable
+			className={twMerge(
+				'w-10 h-10 rounded-xl active:scale-90 transition-all duration-200',
+				className,
+				editable && pressableEditableClassNames
+			)}
+			style={[
+				{ backgroundColor: value },
+				style,
+				editable && pressableEditableStyles
+			]}
+			{...props}
+		>
+			{editable && (
+				<View
+					className="rounded transition-colors duration-200"
+					style={{
+						width: '85%',
+						height: '85%',
+						alignSelf: 'center',
+						backgroundColor: value
+					}}
+				/>
+			)}
+		</Pressable>
+	)
+}
