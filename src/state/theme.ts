1cabf9555c8b476c04f28eb35088253c5db08192
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-03-01 12:44:04 -0500

    Add themes to global state and load them from AsyncStorage

--- src/state/theme.ts
+++ src/state/theme.ts
@@ -1,21 +1,27 @@
 import { create } from 'zustand'
-import type { Theme } from '@/types/theme'
+import { THEMES } from '@/constants/theme'
+import type { Theme, ThemeObject } from '@/types/theme'
 import { getItem, saveItem } from '@/utils/asyncStorage'
 
 interface ThemeStore {
 	theme: Theme
+	themes: Record<string, ThemeObject>
 	setTheme: (theme: Theme) => Promise<void>
 	load: () => Promise<void>
 }
 
 export const useTheme = create<ThemeStore>((set) => ({
 	theme: 'dark',
+	themes: {},
 	setTheme: async (theme) => {
 		await saveItem({ name: 'theme', value: theme })
 		set({ theme })
 	},
 	load: async () => {
+		const themes = await getItem<Record<string, ThemeObject>>({
+			name: 'themes'
+		})
 		const theme = (await getItem<Theme>({ name: 'theme' })) || 'dark'
-		set({ theme })
+		set({ theme, themes: Object.assign({}, THEMES, themes ?? {}) })
 	}
 }))
