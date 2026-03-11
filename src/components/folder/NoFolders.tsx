24d27ea1bbe9b3ecb59ffa920fd3e9b4a47c4371
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-03-11 11:13:23 -0500

    Change styles and remove unnecessary opacity animation

--- src/components/folder/NoFolders.tsx
+++ src/components/folder/NoFolders.tsx
@@ -1,42 +1,19 @@
 import { IconFolderPlus } from '@tabler/icons-react-native'
-import { useCallback, useEffect } from 'react'
-import { Animated, Pressable, Text, useAnimatedValue } from 'react-native'
+import { useCallback } from 'react'
+import { Animated, Pressable, Text } from 'react-native'
 import { StyledPressable } from '@/components/layout/StyledPressable'
-import useFolder from '@/state/Folder'
 import { useModal } from '@/state/modal'
 import { getThemeColor } from '@/utils/theme'
 
 export function NoFolders() {
 	const { openModal } = useModal()
-	const { folders } = useFolder()
-
-	const thereIsFolders = folders.length > 0
-	const opacityValue = useAnimatedValue(thereIsFolders ? 1 : 0)
-
-	useEffect(() => {
-		const toValue = thereIsFolders ? 0 : 1
-
-		const opacityAnimation = Animated.timing(opacityValue, {
-			toValue,
-			duration: 200,
-			useNativeDriver: true
-		})
-
-		opacityAnimation.start()
-	}, [opacityValue, thereIsFolders])
 
 	const handleClickOpenModal = useCallback(() => {
 		openModal('folder')
 	}, [openModal])
 
 	return (
-		<Animated.View
-			className="items-center mt-36 w-11/12 mx-auto py-10 px-4 rounded-lg shadow dark:shadow-white/70 shadow-gray-300/30"
-			style={{
-				opacity: opacityValue,
-				backgroundColor: getThemeColor('surface-soft')
-			}}
-		>
+		<Animated.View className="items-center mt-36 w-11/12 mx-auto py-10 px-4 rounded-lg">
 			<Pressable
 				className="flex-row items-center justify-center rounded-full p-7"
 				style={{
