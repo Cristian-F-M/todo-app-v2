ff03c5691eed6cb9b134dfbbdc629cc48500ab9f
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-03-13 18:38:41 -0500

    Fix styles

--- src/components/layout/BackgroundIcon.tsx
+++ src/components/layout/BackgroundIcon.tsx
@@ -3,14 +3,11 @@ import CMLogo from '@/icons/CMLogo'
 import { getThemeColor, useThemeStyles } from '@/utils/theme'
 
 export function BackgroundIcon() {
-	const [backgroundColor, iconColor] = useThemeStyles(() => [
-		getThemeColor('background'),
-		getThemeColor('primary', 0.4)
-	])
+	const [iconColor] = useThemeStyles(() => [getThemeColor('primary', 0.4)])
 
 	return (
 		<View
-			style={{ backgroundColor, filter: 'blur(10px)' }}
+			style={{ filter: 'blur(10px)' }}
 			className="absolute w-full h-full flex items-center justify-center"
 		>
 			<CMLogo width={200} height={200} color={iconColor} />
