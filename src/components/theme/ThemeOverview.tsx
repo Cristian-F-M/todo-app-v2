e8d8a2b56dfd9a77903f3c7a9f2d53cf6e47e9cf
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-03-13 14:35:06 -0500

    Use marque animation to show each theme color instead of use Flatlist component

--- src/components/theme/ThemeOverview.tsx
+++ src/components/theme/ThemeOverview.tsx
@@ -1,4 +1,5 @@
-import { FlatList, Pressable, Text, View } from 'react-native'
+import { Marquee } from '@animatereactnative/marquee'
+import { Text, View } from 'react-native'
 import { twMerge } from 'tailwind-merge'
 import type { Theme, ThemeParsedObject } from '@/types/theme'
 import { getThemeColor } from '@/utils/theme'
@@ -12,7 +13,7 @@ export function ThemeOverview({
 	isSelected?: boolean
 }) {
 	return (
-		<Pressable
+		<View
 			className={twMerge(
 				'flex flex-row justify-between items-center mt-2 py-3 px-3 rounded-lg border-2 border-transparent'
 			)}
@@ -36,24 +37,25 @@ export function ThemeOverview({
 				</Text>
 			</View>
 			<View className="w-1/12 h-full" />
-			<FlatList
-				className="pb-4"
-				indicatorStyle="white"
-				horizontal
-				showsHorizontalScrollIndicator={true}
-				nestedScrollEnabled
-				data={Object.values(theme.colors)}
-				renderItem={({ item }) => (
-					<View
-						className="w-10 h-10 mr-1 rounded-md"
-						style={{ backgroundColor: `rgb(${item})` }}
-					/>
-				)}
-				initialNumToRender={5}
-				maxToRenderPerBatch={3}
-				windowSize={3}
-				removeClippedSubviews={true}
-			/>
-		</Pressable>
+			<View className="w-6/12">
+				<Marquee
+					speed={0.5}
+					style={{
+						overflow: 'hidden'
+					}}
+					spacing={4}
+				>
+					<View className="flex-row gap-x-1">
+						{Object.entries(theme.colors).map(([key, value]) => (
+							<View
+								key={key}
+								className="w-10 h-10 rounded-md"
+								style={{ backgroundColor: `rgb(${value})` }}
+							/>
+						))}
+					</View>
+				</Marquee>
+			</View>
+		</View>
 	)
 }
